d54de98da511c56d33d8f9c25124890cb5e3336b
Ritikaa034 <145450816+Ritikaa034@users.noreply.github.com>
2024-03-06 16:25:54 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ritika-backend-tfstate-bucket-eks" # Replace with your actual S3 bucket name
+    bucket = "terraform-statefile-ritika" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "ap-south-1"
   }
 }
